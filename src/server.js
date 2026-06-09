e763e818c8e580f343d5619793694c9ba60a0702
yalim-digital <lyiamdev8@gmail.com>
2026-06-09 22:11:14 +0300

    correction mailer

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const http = require('http');
 const app = require('./app');
 
 const server = http.createServer(app);
+const db = require('./config/database');
 
 const socket = require('./sockets/reunion.socket');
 
@@ -17,5 +18,13 @@ server.listen(process.env.PORT, () => {
     console.log(
         `Serveur lancé sur le port ${process.env.PORT}`
     );
+setInterval(async () => {
+  try {
+    await db.query("SELECT 1"); // ping MySQL
+    console.log("Keep-alive DB OK");
+  } catch (err) {
+    console.error("Keep-alive error:", err.message);
+  }
+}, 60 * 1000);
 
 });
