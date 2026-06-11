a717dacb9fccbaa056725a8e2ab6e11eb4477b66
yalim-digital <lyiamdev8@gmail.com>
2026-06-11 14:30:51 +0300

    endpoint register

--- src/utils/matricule.js
+++ src/utils/matricule.js
@@ -6,7 +6,7 @@ const generateMatricule = () => {
     const year =
         new Date().getFullYear();
 
-    const random =new Date().getMonth()+'-'+new Date().getDay()+'-'+new Date().getHours()+'-'+new Date().getMinutes()+'-'+new Date().getSeconds();
+    const random =new Date().getMonth()+''+new Date().getDay()+''+new Date().getHours()+''+new Date().getMinutes()+''+new Date().getSeconds();
     console.log(random);
 
     return `IDEM-${year}-${random}`; 
